d84861a3b89d85c340de146e39a09ec5d3a142b0
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2023-04-17 11:17:06 +0530

    Updating Account and Transaction model

--- src/main/java/com/bhavesh/accountmanagement/Model/Transaction.java
+++ src/main/java/com/bhavesh/accountmanagement/Model/Transaction.java
@@ -1,19 +1,32 @@
 package com.bhavesh.accountmanagement.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "transaction")
 public class Transaction {
+//     (Transaction ID, Transaction reference number, Date Time,
+//    Type (Debit/Credit), SubType (Cash/Transfer), Current Balance
     @Id
-    @Column(name = "id", nullable = false)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer transactionId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long transactionReferenceNumber;
+    private Long refNumber;
+    private LocalDateTime transactionDate;
+    @Column(columnDefinition = "enum('Debit', 'Credit')")
+    private String type;
+    @Column(columnDefinition = "enum('Cash', 'Transfer')")
+    private String subType;
+    @ManyToOne
+    private Account account;
 
 }
