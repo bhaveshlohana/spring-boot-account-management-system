4dce51c0bdd74f2a53fb03b3108c698aed1361bb
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2023-04-17 14:13:22 +0530

    Account model

--- src/main/java/com/bhavesh/accountmanagement/Model/Account.java
+++ src/main/java/com/bhavesh/accountmanagement/Model/Account.java
@@ -1,11 +1,15 @@
 package com.bhavesh.accountmanagement.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "account")
 public class Account {
