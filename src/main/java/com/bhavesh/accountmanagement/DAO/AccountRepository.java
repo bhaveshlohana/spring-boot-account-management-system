ea60652800a8793f05bf08a2900749ae440d54aa
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2023-04-17 19:34:24 +0530

    Adding specific methods to find

--- src/main/java/com/bhavesh/accountmanagement/DAO/AccountRepository.java
+++ src/main/java/com/bhavesh/accountmanagement/DAO/AccountRepository.java
@@ -3,5 +3,8 @@ package com.bhavesh.accountmanagement.DAO;
 import com.bhavesh.accountmanagement.Model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    List<Account> findByCustomerId(int customerId);
 }
