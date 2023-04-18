b896239e220726272d5022a621a1fc6dc9eb6861
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2023-04-18 14:38:09 +0530

    @Query wasn't working, switched to some other resort to get the sum of amount withdrawn in a day.

--- src/main/java/com/bhavesh/accountmanagement/DAO/TransactionRepository.java
+++ src/main/java/com/bhavesh/accountmanagement/DAO/TransactionRepository.java
@@ -2,9 +2,15 @@ package com.bhavesh.accountmanagement.DAO;
 
 import com.bhavesh.accountmanagement.Model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     List<Transaction> findByAccountAccountNumber(int accountNumber);
+
+//    @Query("SELECT t.* FROM transaction as t WHERE account_account_number=:accountNumber AND DATE(t.transaction_date) BETWEEN :startDate AND DATE_ADD(:endDate, INTERVAL 1 DAY)")
+//    List<Transaction> findByTransactionDateBetween(@Param("accountNumber") int accountNumber, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
